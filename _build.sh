a31c479deef4eb6f780afe6b14bfba3b6b189863
Shyam Saladi <saladi@caltech.edu>
2020-11-18 17:11:43 -0800

    xelatex missing on gh actions... figure this out later

--- _build.sh
+++ _build.sh
@@ -3,5 +3,7 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::bs4_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+
+# Figure this out on gh actions later...
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 
