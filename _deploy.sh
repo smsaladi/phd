4faa0ecc7c28f3ae92a0ecac3228cebb88e86909
Shyam Saladi <saladi@caltech.edu>
2020-11-18 17:31:58 -0800

    try to fix deploy (token issue?)

--- _deploy.sh
+++ _deploy.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-[ -z "${GITHUB_TOKEN}" ] && exit 0
-
 git config --global user.email "saladi@caltech.edu"
 git config --global user.name "Shyam Saladi"
 
-git clone -b gh-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git book-output
+git clone -b gh-pages https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git book-output
 cd book-output
 
 cp -r ../_book/* ./
